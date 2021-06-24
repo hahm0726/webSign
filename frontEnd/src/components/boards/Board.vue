9f19493deab967c8ddfaf0854eee447452273699
hahm0726 <hahm0726@naver.com>
2021-06-25 04:11:40 +0900

    Board 컴포넌트 생성 - 명단 리스트 제공과 서명받기 기능 구현

---
+++ frontEnd/src/components/boards/Board.vue
@@ -0,0 +1,164 @@
+<template>
+  <v-data-table class="tables" :headers="thItems" :items="tdItems">
+    <template v-slot:top>
+      <v-dialog v-model="dialog" persistent max-width="500px">
+        <v-card class="pa-2">
+          <vue-signaturePad
+            class="canvas-color"
+            width="100%"
+            height="200px"
+            ref="signaturePad"
+            :options="{
+              onBegin: () => {
+                $refs.signaturePad.resizeCanvas();
+              },
+            }"
+          >
+          </vue-signaturePad>
+
+          <v-btn class="my-2" small @click="clearCanvas">지우기</v-btn>
+
+          <v-card-actions class="px-0">
+            <v-spacer></v-spacer>
+            <v-btn color="error white--text" small @click="closeDialog">
+              취소
+            </v-btn>
+            <v-btn color="blue darken-1 white--text" small @click="closeDialog">
+              서명완료
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </template>
+
+    <template v-slot:[`item.receivedDate`]="{ item }">
+      {{ receivedDate(item.receivedDate) }}
+    </template>
+    <template v-slot:[`item.state`]="{ item }">
+      {{ isReceived(item.state) }}
+    </template>
+    <template v-slot:[`item.action`]="{ item }">
+      <v-btn v-if="item.state" @click="openDialog(item)">서명확인</v-btn>
+      <v-icon v-else :disabled="item.state" @click="openDialog(item)">
+        fa-file-signature
+      </v-icon>
+    </template>
+  </v-data-table>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    dialog: false,
+    thItems: [
+      { text: "연번", value: "idx", align: "center" },
+      { text: "이름", value: "name", align: "center" },
+      { text: "거주동", value: "location", align: "center" },
+      { text: "수량", value: "amount", align: "center" },
+      { text: "수령일", value: "receivedDate", align: "center" },
+      { text: "수령상태", value: "state", align: "center" },
+      { text: "작업", value: "action", align: "center" },
+    ],
+    tdItems: [
+      {
+        idx: 1,
+        name: "홍길동",
+        location: "안양2동",
+        amount: 1,
+        receivedDate: null,
+        state: true,
+      },
+      {
+        idx: 1,
+        name: "홍길동",
+        location: "안양2동",
+        amount: 1,
+        receivedDate: null,
+        state: true,
+      },
+      {
+        idx: 1,
+        name: "홍길동",
+        location: "안양2동",
+        amount: 1,
+        receivedDate: null,
+        state: true,
+      },
+      {
+        idx: 1,
+        name: "홍길동",
+        location: "안양2동",
+        amount: 1,
+        receivedDate: null,
+        state: true,
+      },
+      {
+        idx: 1,
+        name: "홍길동",
+        location: "안양2동",
+        amount: 1,
+        receivedDate: null,
+        state: true,
+      },
+      {
+        idx: 2,
+        name: "김상만",
+        location: "안양1동",
+        amount: 2,
+        receivedDate: null,
+        state: false,
+      },
+      {
+        idx: 3,
+        name: "최길자",
+        location: "안양1동",
+        amount: 1,
+        receivedDate: null,
+        state: false,
+      },
+    ],
+  }),
+
+  methods: {
+    // 수령 상태 출력 변환
+    isReceived: function (val) {
+      if (val === true) return "수령완료";
+      return "수령 전";
+    },
+    //수령날짜 출력 변환
+    receivedDate: function (val) {
+      if (val === null) return "수령 전";
+      return val;
+    },
+    //다이얼로그 열기
+    openDialog() {
+      this.dialog = true;
+    },
+    //다이얼로그 닫기
+    closeDialog() {
+      this.dialog = false;
+      this.clearCanvas();
+    },
+
+    clearCanvas() {
+      this.$refs.signaturePad.clearSignature();
+    },
+    save() {
+      const { isEmpty, data } = this.$refs.signaturePad.saveSignature();
+      console.log(isEmpty);
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.tables {
+  width: 100%;
+  max-width: 1200px;
+}
+.canvas-color {
+  background-color: lightgray;
+  border: 1px solid black;
+}
+</style>
---
+++ frontEnd/src/components/layout/MainLayout.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-app id="app">
+    <header id="app-header">
+      <span>인수증</span>
+    </header>
+    <v-main>
+      <v-container id="contents-wrap" class="pt-2">
+        <component :is="selectedComponent"></component>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Board from "/src/components/boards/Board";
+
+export default {
+  components: {
+    Board,
+  },
+  computed: {
+    //선택된 컴포넌트 반환
+    selectedComponent: function () {
+      return Board;
+    },
+  },
+};
+</script>
+
+<style scoped>
+#app-header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 10vh;
+  border: 2px solid;
+}
+#app {
+  padding: 0;
+  width: 100%;
+}
+#contents-wrap {
+  min-height: 90vh;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
